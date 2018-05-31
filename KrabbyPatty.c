2aec6f56b1a56ff648c43c846830f73d82777b5a
ThePStar7 <39674602+ThePStar7@users.noreply.github.com>
2018-05-31 09:40:46 +0200

    Add files via upload

---
+++ KrabbyPatty.c
@@ -0,0 +1,19 @@
+/* lib1 Source Code
+Copyright PS7
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+char menuitem[] = "Krabby Patty";
+int quant;
+char pints [10];
+float price = 2.99; //The price of a Krabby Patty
+
+printf("How many Krabby Patty's do you want?");
+gets(pints);
+quant=atoi(pints);
+printf("That'd be $%.2f please.\n",quant*price);
+}
